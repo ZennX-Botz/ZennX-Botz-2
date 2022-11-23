08029ce982eefb408638cd4214d920a6c2eb32c8
ZennX-Botz <117031895+ZennX-Botz@users.noreply.github.com>
2022-11-23 16:17:47 +0700

    Update info-rules.js

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -12,12 +12,12 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let tqto = `*Rules Bot*:
+let tqto = `*RULLES BERMAIN BOT WA*:
 
 1. Dilarang telp/vc bot
 2. Harap beri jeda saat menggunakan bot (jangan spam)
 3. Dilarang mengeksploitasi bot
-4. Dilarang menjual belikan bot, sebab bot ini free untuk digunakan (kecuali memasukkan ke dalam group harus izin owner!)
+4. Dilarang mendownload file audio yang berdurasi di atas 50 menit
 5. Bot tidak menyimpan data user
 6. Kami tidak bertanggung jawab atas apa yg user lakukan kepada bot ataupun sebaliknya
 7. Hindari konten pornografi (berupa gambar, dokumen, video, dan stiker)
@@ -34,21 +34,21 @@ let tqto = `*Rules Bot*:
  
  Syarat Ketentuan Bot 
  1. Bot akan keluar dari group apabila sudah waktunya keluar. 
- 2. bot dapan mem-ban users secara sepihak terlepas dari users salah atau tidak. 
- 3. bot *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.* 
- 4. bot akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan. 
- 5. bot bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+ 2. bot dapat membanned user secara sepihak terlepas dari users salah atau tidak. 
+ 3. bot *tidak akan bertanggungjawab atas apapun yang user lakukan terhadap fitur bot.* 
+ 4. bot akan memberikan hukuman berupa block atau ban terhadap users yang melanggar peraturan. 
+ 5. bot bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
 Tahulah berterimakasih sebab anda tidak perlu membayar untuk menggunakan bot ini, gunakan secara bijak, jangan berlebihan.
 
 Be a smart user and good luck!
-*©Kebijakan By RullBot*
+*©Kebijakan By ZennX-Botz*
 `
 conn.sendButtonDoc(m.chat, tqto, wm,'Saya Paham','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/khoirul36_",
+    mediaUrl: "https://Instagram.com/karfuanyogich",
     mediaType: "VIDEO",
-    description: "https://Instagram.com/khoirul36_", 
-    title: 'RullBOT',
+    description: "https://Instagram.com/karfuanyogich", 
+    title: 'Patuhi Rullesnya Ya!',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
